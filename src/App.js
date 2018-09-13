9b99f5c43eb65603e6fe8b83cf4182e71cd6b798
Pierre Sisson <pierre.sisson@gmail.com>
2018-09-13 17:18:35 +0200

    Add form and input button to search an article

--- src/App.js
+++ src/App.js
@@ -2,10 +2,48 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ''
+        };
+
+        // this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleSubmitRandom = this.handleSubmitRandom.bind(this);
+    }
+    
+    // handleChange(event) {
+    //     this.setState({value: event.target.value});
+    //     console.log("ok");
+    // }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log("ok");
+    }
+
+    handleSubmitRandom(event) {
+        const random = "https://en.wikipedia.org/wiki/Special:Random";
+        console.log(random);
+    }
+
 	render() {
 		return (
 			<div>
-				<h1 class="main-title">Hello World</h1>
+                <h1 class="main-title">Hello World</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Name:
+                        <input placeholder="Search" type="text" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+                <button
+                    value="Random Article"
+                    className="btn btn-submit"
+                    onClick={this.handleSubmitRandom}>
+                </button>
 			</div>
 		);
 	}
